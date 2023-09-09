83cbe5dcf5b12e868e01025b8ad9c9f6b68ee842
vphucnn <140361269+vphucnn@users.noreply.github.com>
2023-09-09 18:20:53 +0700

    add ui create erc20

--- apps/web/src/views/CoinTools/index.tsx
+++ apps/web/src/views/CoinTools/index.tsx
@@ -1,8 +1,26 @@
-import Page from '../Page'
 import { useAccount, useBalance } from 'wagmi'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import Typography from '@mui/material/Typography';
+import Switch from '@mui/material/Switch';
+import Button from '@mui/material/Button';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function CoinTools() {
+  const [name, setName] = React.useState('Cat in the Hat');
+  const [checked, setChecked] = React.useState(true);
 
   const { address } = useAccount()
 
@@ -10,13 +28,146 @@ export default function CoinTools() {
     address: address,
   })
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
   if (isLoading) return <div>Fetching balance…</div>
   if (isError) return <div>Error fetching balance</div>
   return (
-    <Page>
-      <div>
-        Balance: {data?.formatted} {data?.symbol}
-      </div>
-    </Page>
+    <Paper >
+      <Box sx={{ flexGrow: 1, p: 2, minHeight: '100vh' }}
+      >
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="h5"
+            component="h2" sx={{
+              color: 'teal',
+            }}>
+            Balance: {data?.formatted} {data?.symbol}
+          </Typography>
+        </Grid>
+        <Grid sx={{ p: 5 }} container spacing={10}>
+          <Grid xs={12} md={6}>
+            <h2>Basic setting</h2>
+            <Grid sx={{ mt: 2 }}>
+              <InputLabel shrink htmlFor="bootstrap-input">
+                Token name
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                value={name}
+                variant="outlined"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setName(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <InputLabel shrink htmlFor="bootstrap-input">
+                Token name
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                value={name}
+                variant="outlined"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setName(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid sx={{ mt: 2 }} columns={16} container spacing={2}>
+              <Grid xs={9} md={8} >
+                <InputLabel shrink htmlFor="bootstrap-input">
+                  Initial supply
+                </InputLabel>
+                <TextField
+                  size="small"
+                  fullWidth
+                  id="standard-read-only-input"
+                  defaultValue="Hello World"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid xs={1} md={4}  ></Grid>
+              <Grid xs={6} md={4}  >
+                <InputLabel shrink htmlFor="bootstrap-input">
+                  Decimals (0-18)
+                </InputLabel>
+                <TextField
+                  size="small"
+                  fullWidth
+                  id="standard-read-only-input"
+                  defaultValue="Hello World"
+                  variant="outlined"
+                />
+              </Grid>
+
+            </Grid>
+          </Grid>
+          <Grid xs={12} md={5}>
+            <h2>Token configuration</h2>
+
+            <Grid sx={{ mt: 2 }}>
+              <Switch
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+              <span>Can Burn</span>
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <Switch
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+              <span>Can Mint</span>
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <Switch
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+              <span>Can Pause</span>
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <Switch
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+              <span>Can Blacklist</span>
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <Switch
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+              <span>Apply Burn Fee (Deflationary token
+                )</span>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button variant="contained">Create</Button>
+        </Grid>
+      </Box>
+    </Paper>
   )
 }
