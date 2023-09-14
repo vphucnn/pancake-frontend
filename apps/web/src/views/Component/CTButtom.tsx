e4b9bdeadac669e76711999f891f6e4109a29907
vphucnn <140361269+vphucnn@users.noreply.github.com>
2023-09-14 23:37:57 +0700

    edit menu

--- apps/web/src/views/Component/CTButtom.tsx
+++ apps/web/src/views/Component/CTButtom.tsx
@@ -3,10 +3,11 @@ import { styled } from '@mui/material/styles'
 
 const CTButtom = styled(Button)<ButtonProps>(({ theme }) => ({
   '&.MuiButtonBase-root:hover': {
-    background: `linear-gradient(180deg, rgba(88, 74, 37, 0.8) 0%, rgba(0, 0, 0, 0.8) 100%), linear-gradient(180deg, #FFF3BF 0%, #E8CC93 45.83%, #BCA578 100%)`,
-    'box-shadow': `0px 4px 44px rgba(143, 101, 59, 0.2)`
+
   },
   '&.MuiButtonBase-root': {
+    background: `linear-gradient(180deg, rgba(88, 74, 37, 0.8) 0%, rgba(0, 0, 0, 0.8) 100%), linear-gradient(180deg, #FFF3BF 0%, #E8CC93 45.83%, #BCA578 100%)`,
+    'box-shadow': `0px 4px 44px rgba(143, 101, 59, 0.2)`,
     border: "0 solid",
     boxSizing: "border-box",
     borderStyle: "solid",
@@ -14,8 +15,7 @@ const CTButtom = styled(Button)<ButtonProps>(({ theme }) => ({
     "--tw-border-opacity": "1",
     borderColor: "rgb(230 203 130 / var(--tw-border-opacity))",
     borderWidth: "1px",
-    "--tw-bg-opacity": "0.8",
-    backgroundColor: "rgb(98 86 55/var(--tw-bg-opacity))"
+    "--tw-bg-opacity": "0.8"
     ,
 
     '&.MuiInputBase-root fieldset': {
--- packages/uikit/src/widgets/Menu/Menu.tsx
+++ packages/uikit/src/widgets/Menu/Menu.tsx
@@ -159,7 +159,7 @@ const Menu: React.FC<React.PropsWithChildren<NavProps>> = ({
                   <AtomBox mr="12px" display={{ xs: "none", lg: "block" }}>
                     <CakePrice chainId={chainId} showSkeleton={false} cakePriceUsd={cakePriceUsd} />
                   </AtomBox>
-                  <Box mt="4px">
+                  {/* <Box mt="4px">
                     <LangSelector
                       currentLang={currentLang}
                       langs={langs}
@@ -168,7 +168,7 @@ const Menu: React.FC<React.PropsWithChildren<NavProps>> = ({
                       color="textSubtle"
                       hideLanguage
                     />
-                  </Box>
+                  </Box> */}
                   {rightSide}
                 </Flex>
               </StyledNav>
